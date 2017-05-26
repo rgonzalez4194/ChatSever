d484ab54e2b5121cff866174c6ac16d364a9fddc
Rob <Rob@Rob-PC>
2017-05-25 20:05:25 -0500

    Commented

--- Chat 4/src/Client.java
+++ Chat 4/src/Client.java
@@ -5,7 +5,7 @@ import java.io.BufferedInputStream;
 	import java.net.ServerSocket;
 	import java.net.Socket;
 public class Client implements Runnable{
-	// http://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+	
 	private Socket s; 
 	private DataInputStream dis;
 	private DataInputStream user;
@@ -13,7 +13,7 @@ public class Client implements Runnable{
 	private boolean running; 
 	private String name;
 
-
+		//Constructor, takes host ip and a port
 		public Client(String host, int port)
 		{
 			running = true;
@@ -21,20 +21,25 @@ public class Client implements Runnable{
 				//connects to server
 				s = new Socket(host, port);
 				dis = new DataInputStream(new BufferedInputStream (s.getInputStream()));
+				//starts recieving messages
 				receive(); 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.out.println("Could not connect to Server.");
 			} 
 		}
 		
+		//Constructor, takes socket
 		public Client(Socket socket)
 		{
+			//sets socket
 			this.s = socket;
 			running = true;
 			try {
+			//creates input and output streams 
 				user = new DataInputStream(System.in);
 				dos = new DataOutputStream(s.getOutputStream());
+				//asks for and sets username
 				System.out.print("Please enter your username: ");
 				name = user.readLine();
 			} catch (IOException e) {
@@ -44,6 +49,8 @@ public class Client implements Runnable{
 			
 		}
 		
+		//run method runs when thread is .start-ed
+		//reads input from user and then sends it
 		public void run()
 		{
 			while(running)
@@ -54,13 +61,16 @@ public class Client implements Runnable{
 					dos.flush(); 
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					
 				}
 				
 			}
 		}
 		
+		
+		//Receives messages from server
 		public void receive(){
+			//creates a thread to receive incoming messages
 			new Thread(new Client(s)).start();
 			while(running)
 			{
@@ -68,15 +78,22 @@ public class Client implements Runnable{
 					System.out.println(dis.readUTF());
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					System.out.println("Server has closed.");
+					running = false;
+					}
 				}
 			}
-		}
 		
+		
+		
+		//Main method, accepting ip and port number
 		public static void main(String[] args)
 		{
+			//sets port number
 			int port = Integer.parseInt(args[1]);
+			//sets host ip
 			String host = args[0];
+			//creates new client 
 			Client client = new Client(host,port); 
 		}
 		
--- Chat 4/src/ThreadServer.java
+++ Chat 4/src/ThreadServer.java
@@ -17,10 +17,12 @@ public class ThreadServer implements Runnable{
 	private boolean running;  
 	private Socket s;
 
+	//Constructor, creates new array of threads 
 	public ThreadServer(){
 		threads = new ArrayList<ThreadServer>();
 	}
 
+	//Super Constructor, Sets socket, tells us our server is running, and passes in current array of threads
 	public ThreadServer(Socket s,ArrayList<ThreadServer> threads)
 	{
 		this.s = s;
@@ -28,21 +30,23 @@ public class ThreadServer implements Runnable{
 		this.threads = threads;
 	}
 	
+	//run method runs when you .start a thread
 	public void run(){
 		try { 
+			//Creates new data input and output streams that grab from the socket we have established
+			//input data is buffered
 			dis = new DataInputStream(new BufferedInputStream (s.getInputStream()));
 			dos = new DataOutputStream(s.getOutputStream());
-		
+			
+			//while loop reads messages and sends them to all clients as long as our server is running
 			while(running)
 			{
 				try {
 					String line = dis.readUTF();
 					sendAll(line);
 					//sendMessage(line);
-					
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					stop();
 					running = false;
 				} 
 				
@@ -50,18 +54,33 @@ public class ThreadServer implements Runnable{
 		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-			threads.remove(threads.indexOf(this));
+			stop();
 		} 
 		
 		
 	}
 	
+	//closes a thread 
+	public void stop()
+	{
+		threads.remove(threads.indexOf(this));
+		try {
+			dis.close();
+			dos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//sends message, writes to the output stream and then flushes
 	public void sendMessage(String line) throws IOException{
 		dos.writeUTF(line);
 		dos.flush();
 	}
 	
+	//Sends a message to all created threads in the array of threads
 	public void sendAll(String message) throws IOException{
 		for(int i=0; i<this.threads.size(); i++){
 			ThreadServer thread = this.threads.get(i);
@@ -69,14 +88,19 @@ public class ThreadServer implements Runnable{
 		}
 	}
 	
-	// Creates new threads
+	//runs server
+	//accepts clients into server
 	public void runServer()
 	{
 		running = true;
 		System.out.println("Server running!");
 		while(running){
 			try {
+				//creates new thread 
+				//ss.accept() listens for a connection to the server socket and then returns the socket connected to
+				//also passes in the current array of threads
 				threads.add(new ThreadServer(ss.accept(), threads));
+				//starts the newly created thread
 				new Thread(threads.get(threads.size()-1)).start();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -85,15 +109,19 @@ public class ThreadServer implements Runnable{
 		}
 	}
 	
+	//Main method
 	public static void main(String[] args)
 	{
+		//sets port to be used
 		int port = Integer.parseInt(args[0]);
 		try {
+		//creates a server socket using that port 
 			ss = new ServerSocket(port);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//creates and runs server
 		ThreadServer server = new ThreadServer();
 		server.runServer();
 	}
